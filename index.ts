a4eb2eb10b522f3b12dfcde111c9be1db4235559
Drew Nollsch <drew.nollsch@gmail.com>
2022-06-27 20:27:51 +0200

    initialize server

---
+++ index.ts
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json({ limit: "5MB", extended: true })); // express.json is replacement for bodyparser , Include extra params {} if needed
+app.use(cors());
+app.use(helmet());
+
+app.get("/", (req, res) => {
+  res.send("Hello from express server");
+});
+
+// Routes goes here
+app.listen(port, () => console.log(`Listening on port ${port}.`));
